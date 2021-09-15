bfe4ee346b1e3411ff47f02c3a0524bd13c2c166
arpitdalal <arpitdalalm@gmail.com>
2021-09-15 12:37:37 -0400

    add more checks and change logic

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -19,41 +19,78 @@ export const jobController = (_: Request, res: Response): void => {
   const todaysFormattedDate = moment(todaysDate).format('MMMM D, YYYY');
 
   const ekadashisRawData = fs.readFileSync(
-    path.join(__dirname, '../ekadashis.json'),
+    path.join(__dirname, '../../ekadashis.json'),
   );
-  const ekadashis: [Ekadashi] = JSON.parse(
+  const ekadashis: Ekadashi[] | [] = JSON.parse(
     ekadashisRawData as unknown as string,
   );
 
-  ekadashis.forEach((ekadashi) => {
-    if (todaysFormattedDate == ekadashi.date) {
-      sendNotification(ekadashi)
-        .then(() => {
-          console.log('Notification sent');
+  if (ekadashis.length === 0) {
+    const messageOptions = {
+      from: 'arpitdalalm@gmail.com',
+      to: 'arpitdalalm@gmail.com',
+      subject: 'ERROR MESSAGE FROM EKADASHI REMINDER BOT',
+      text: `ekadashis.json is empty! Visit https://ekadashi-reminder.herokuapp.com/ to add Ekadashis.`,
+    };
+    transporter.sendMail(messageOptions, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
 
-          res.send({
-            ok: 'true',
-          });
-        })
-        .catch((err) => {
-          // SEND AN EMAIL ABOUT THE ERROR TO ARPITDALALM@GMAIL.COM
-          const messageOptions = {
-            from: 'arpitdalalm@gmail.com',
-            to: 'arpitdalalm@gmail.com',
-            subject: 'ERROR MESSAGE FROM EKADASHI REMINDER BOT',
-            text: `Couldn't send the the notification to the channel! ERROR: ${err}!`,
-          };
-          transporter.sendMail(messageOptions, (error) => {
-            if (error) {
-              console.log(error);
-            }
-          });
+    res.send({
+      ok: 'false',
+      message: 'Ekadashis empty',
+    });
+  } else {
+    let isNotificationSent = false;
+    let isError = false;
+    let error;
+    ekadashis.some((ekadashi) => {
+      if (todaysFormattedDate === ekadashi.date) {
+        sendNotification(ekadashi)
+          .then(() => {
+            console.log('Notification sent');
+            isNotificationSent = true;
+            return true;
+          })
+          .catch((err) => {
+            isError = true;
+            error = err;
 
-          res.send({
-            ok: 'false',
-            message: err,
+            // SEND AN EMAIL ABOUT THE ERROR TO ARPITDALALM@GMAIL.COM
+            const messageOptions = {
+              from: 'arpitdalalm@gmail.com',
+              to: 'arpitdalalm@gmail.com',
+              subject: 'ERROR MESSAGE FROM EKADASHI REMINDER BOT',
+              text: `Couldn't send the the notification to the channel! ERROR: ${err}!`,
+            };
+            transporter.sendMail(messageOptions, (error) => {
+              if (error) {
+                console.log(`Email error: ${error}`);
+              }
+            });
           });
+      }
+    });
+
+    if (!isNotificationSent) {
+      res.send({
+        ok: 'true',
+        message: 'Not an Ekadashi day',
+      });
+    } else {
+      if (!isError) {
+        res.send({
+          ok: 'true',
+          message: 'Notification sent',
         });
+      } else {
+        res.send({
+          ok: 'false',
+          message: `Error: ${error}`,
+        });
+      }
     }
-  });
+  }
 };
